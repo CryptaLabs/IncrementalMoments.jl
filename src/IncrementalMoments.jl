7b90ff5a80c3372b80b4dceca2a64df46e368912
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2017-06-19 09:54:58 +0100

    Adds incremental moments for arrays

--- src/IncrementalMoments.jl
+++ src/IncrementalMoments.jl
@@ -1,6 +1,8 @@
 module IncrementalMoments
 using Combinatorics: multinomial
+using ArgCheck
 import StatsBase: moment
+
 export update_moments, update_moments!, IncrementalMoment
 export update!, update
 
@@ -9,8 +11,20 @@ export update!, update
 Object to compute moments of a streaming distribution
 """
 abstract IncrementalMoment
+
+"""
+Incremental moment specialized for scalar inputs.
+"""
 type ScalarIncrementalMoment{T <: AbstractFloat} <: IncrementalMoment
     moments::Vector{T}
+    nvalues::Int64
+end
+
+"""
+Incremental moments specialized for array inputs
+"""
+type ArrayIncrementalMoment{T <: AbstractArray} <: IncrementalMoment
+    moments::T
     nvalues::Int64
 end
 
@@ -18,12 +32,23 @@ end
     ScalarIncrementalMoment(zeros(typeof(one(T) / 2), order), 0)
 end
 
+(::Type{IncrementalMoment}){T <: AbstractFloat, I <: Integer}(::Type{T},
+                                                              dims::NTuple{I},
+                                                              order=2) = begin
+    ArrayIncrementalMoment(zeros(typeof(one(T) / 2), (dims..., order)), 0)
+end
+
 update_moments(n::Integer, value::Real, moments::AbstractVector) = begin
     update_moments!(n, value, moments, deepcopy(moments))
 end
+update_moments(n::Integer, value::AbstractArray, moments::AbstractArray) = begin
+    update_moments!(n, value, moments, deepcopy(moments))
+end
 
-update_moments!{T <: AbstractFloat}(n::Integer, value::Real,
-                                    moments::AbstractVector{T}) = begin
+update_moments!(n::Integer, value::Real, moments::AbstractVector) = begin
+    update_moments!(n, value, moments, moments)
+end
+update_moments!(n::Integer, value::AbstractArray, moments::AbstractArray) = begin
     update_moments!(n, value, moments, moments)
 end
 
@@ -32,7 +57,7 @@ update_moments!{T <: AbstractFloat}(n::Integer, value::Real,
                                     out::AbstractVector{T}) = begin
     length(moments) == 0 && return out
 
-    δ = value .- moments[1]
+    δ = value - moments[1]
     δ_div_n = δ/n
 
     out[1] = moments[1] + δ_div_n
@@ -50,7 +75,41 @@ update_moments!{T <: AbstractFloat}(n::Integer, value::Real,
     out
 end
 
-update!(m::IncrementalMoment, value) = begin
+@inline slice_last(a, n) = begin
+    view(a, collect(1:u for u in size(a)[1:end-1])..., n)
+end
+
+update_moments!{T1 <: AbstractFloat, T2 <:AbstractFloat}(
+                n::Integer, value::AbstractArray{T1},
+                moments::AbstractArray{T2},
+                out::AbstractArray{T2}) = begin
+
+    @argcheck ndims(value) + 1 == ndims(moments)
+    @argcheck size(value) == size(moments)[1:end - 1]
+    @argcheck size(moments) == size(out)
+    length(moments) == 0 && return out
+
+    δ = value .- slice_last(moments, 1)
+    δⁿ = δ .* δ
+    δ_div_n = δ/n
+
+    slice_last(out, 1) .= slice_last(moments, 1) .+ δ_div_n
+    size(moments, ndims(moments)) == 1 && return out
+
+    for p in 2:size(moments, ndims(moments))
+        result = slice_last(moments, p)
+        factor = copy(δ_div_n)
+        for k in 1:(p - 2)
+            result .-= multinomial(k, p - k) .* factor .* slice_last(moments, p - k)
+            factor .*= δ_div_n
+        end
+        slice_last(out, p) .= result .+ δⁿ .- δ.*factor
+        δⁿ .*= δ
+    end
+    out
+end
+
+update!(m::ScalarIncrementalMoment, value) = begin
     if m.nvalues == 0
         m.nvalues = 1
         m.moments[1] = value
@@ -61,14 +120,45 @@ update!(m::IncrementalMoment, value) = begin
     m
 end
 
+
 update(m::IncrementalMoment, value) = update!(deepcopy(m), value)
-Base.mean(m::IncrementalMoment) = m.moments[1]
-Base.var(m::IncrementalMoment; corrected=true) = begin
+Base.mean(m::ScalarIncrementalMoment) = m.moments[1]
+Base.var(m::ScalarIncrementalMoment; corrected=true) = begin
     m.moments[2] / (corrected ? m.nvalues - 1: m.nvalues)
 end
-moment(m::IncrementalMoment, n::Integer) = begin
+moment(m::ScalarIncrementalMoment, n::Integer) = begin
     n == 1 ? convert(eltype(m.moments), 0): m.moments[n] / m.nvalues
 end
-Base.size(m::IncrementalMoment) = length(m.moments)
+Base.length(m::ScalarIncrementalMoment) = length(m.moments)
+Base.size(m::ScalarIncrementalMoment) = length(m.moments)
+Base.size(m::IncrementalMoment) = size(m.moments)
+Base.size(m::IncrementalMoment, i::Integer) = size(m.moments, i)
+Base.length(m::IncrementalMoment, i::Integer) = length(m.moments)
+Base.ndims(::ScalarIncrementalMoment) = 1
+Base.ndims(m::ArrayIncrementalMoment) = ndims(m.moments)
+Base.mean(m::ArrayIncrementalMoment) = copy(slice_last(m.moments, 1))
+Base.var(m::ArrayIncrementalMoment; corrected=true) = begin
+    slice_last(m.moments, 2) / (corrected ? m.nvalues - 1: m.nvalues)
+end
+moment(m::ArrayIncrementalMoment, n::Integer) = begin
+    if n == 1
+        result = similar(m.moments, size(m.moments)[1:end - 1])
+        fill!(result, 0)
+        result
+    else
+        slice_last(m.moments, n) / m.nvalues
+    end
+end
+update!(m::ArrayIncrementalMoment, value) = begin
+    if m.nvalues == 0
+        m.nvalues = 1
+        slice_last(m.moments, 1) .= value
+    else
+        m.nvalues += 1
+        update_moments!(m.nvalues, value, m.moments)
+    end
+    m
+end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,38 +2,6 @@ using IncrementalMoments
 using Base.Test
 using StatsBase
 
-@testset "Average only" begin
-    values = Float64[5, 6, 5, 7, 8]
-    moments = [values[1]]
-    for i in 2:length(values)
-        @test mean(values[1:i]) ≈ update_moments!(i, values[i], moments)[1]
-    end
-
-    values = rand(Float64, 100)
-    moments = [values[1]]
-    for i in 2:length(values)
-        @test mean(values[1:i]) ≈ update_moments!(i, values[i], moments)[1]
-    end
-end
-
-@testset "Average and variance" begin
-    values = Float64[5, 6, 5, 7, 8]
-    moments = [values[1], 0]
-    for i in 2:length(values)
-        update_moments!(i, values[i], moments)
-        @test mean(values[1:i]) ≈ moments[1]
-        @test var(values[1:i]) ≈ moments[2] / (i - 1)
-    end
-
-    values = rand(Float64, 100)
-    moments = [values[1], 0]
-    for i in 2:length(values)
-        update_moments!(i, values[i], moments)
-        @test mean(values[1:i]) ≈ moments[1]
-        @test var(values[1:i]) ≈ moments[2] / (i - 1)
-    end
-end
-
 @testset "Average and variance and skewness" begin
     values = Float64[5, 6, 5, 7, 8]
     moments = [values[1], 0, 0]
@@ -90,5 +58,31 @@ end
     end
 end
 
+@testset "Check slicing by last dim" begin
+    values = rand(Int64, (10, 10, 6))
+    @test IncrementalMoments.slice_last(values, 1) == values[:, :, 1]
+    @test IncrementalMoments.slice_last(values, 5) == values[:, :, 5]
 
+    values = rand(Int64, (10, 6))
+    @test IncrementalMoments.slice_last(values, 1) == values[:, 1]
+    @test IncrementalMoments.slice_last(values, 5) == values[:, 5]
+end
+
+@testset "Incremental moments over arrays" begin
+    values = rand(Float64, (10, 10, 100))
+    moments = IncrementalMoment(Float64, size(values)[1:end - 1], 4)
+    N = size(moments, ndims(moments))
+    for i in 1:(N - 1)
+        update!(moments, @view values[:, :, i])
+    end
+    for i in N:size(values, ndims(values))
+        update!(moments, @view values[:, :, i])
+        @test mean(values[:, :, 1:i], ndims(values)) ≈ mean(moments)
+        for n in 2:N
+            expected = [moment(values[u, v, 1:i], n)
+                        for u = 1:size(values, 1), v = 1:size(values, 2)]
+            @test expected ≈ moment(moments, n)
+        end
+    end
+end
 
